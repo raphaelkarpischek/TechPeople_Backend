889243886b55946665edcb706b959c078af856ad
raphaelmartineztaking <raphael.martinez@taking.com.br>
2022-03-27 11:10:57 -0300

    Testando base de dados Heroku

--- db/conn.js
+++ db/conn.js
@@ -1,7 +1,12 @@
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize('techpeople', 'root', 'xD@p9425', {
+/*const sequelize = new Sequelize('techpeople', 'root', 'xD@p9425', {
     host: 'localhost',
+    dialect: 'mysql'
+})*/
+
+const sequelize = new Sequelize('heroku_e53f2cec4a48c71', 'bc3a8ad451a401', '688b11b8', {
+    host: 'us-cdbr-east-05.cleardb.net',
     dialect: 'mysql'
 })
 
